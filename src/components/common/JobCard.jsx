354f22edc94f48879f1745cdbeb37b7a5e2f7f35
tejal-supe <supetejalc@gmail.com>
2024-05-05 14:00:47 +0530

    job card ui

--- src/components/common/JobCard.jsx
+++ src/components/common/JobCard.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { Box, Container, Grid, Paper, styled } from "@mui/material";
+import React from "react";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "left",
+  color: theme.palette.text.secondary,
+  borderRadius: "20px",
+  boxShadow: "rgba(0, 0, 0, 0.1) 0px 3px 10px 3px",
+}));
 
 const JobCard = () => {
   return (
-    <div>JobCard</div>
-  )
-}
+    <>
+      <Item>
+        <div className="posted-at">Posted 10 days ago</div>
+        <div className="compnay-details">
+          <div className="logo">
+            <img
+              alt="company-logo"
+              src="https://w7.pngwing.com/pngs/36/959/png-transparent-meta-logo-facebook-social-media-chat-message-communication-icon-thumbnail.png"
+            />
+          </div>
+          <div className="title">
+            <span>fampay</span>
+            <span>Backend Engineer</span>
+            <span>Banglore</span>
+          </div>
+        </div>
+        <p className="estimated-salary">~18-35 LPA</p>
+        <h6>About Company:</h6>
+        <div className="about-company">
+          <b>About us</b>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
+            aperiam distinctio quaerat ipsum dolor voluptate? Possimus ea vitae
+            odit autem facere dignissimos quidem, qui iure labore dolor ab ex
+            voluptates.
+          </p>
+                  <div className="view-job">View Job</div>
+                  <div className="white-linear"></div>
+        </div>
+        <div className="min-exp">
+          <p>Minimum Experience</p>
+          <p>3 years</p>
+        </div>
+        <div className="apply">Easy Apply</div>
+      </Item>
+    </>
+  );
+};
 
-export default JobCard
+export default JobCard;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import FilterUi from '../../components/common/FilterUi'
-import JobCard from '../../components/common/JobCard'
+import { Box, Container, Grid, Paper, styled } from "@mui/material";
+import React from "react";
+import FilterUi from "../../components/common/FilterUi";
+import JobCard from "../../components/common/JobCard";
 
 const Home = () => {
   return (
-      <div >
-          <FilterUi />
-
-          <JobCard />
+    <div>
+      <FilterUi />
+      <Container sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {Array.from(Array(6)).map((_, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              
+              <JobCard />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
